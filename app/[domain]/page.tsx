286ad7148391c76c61a15bf38ac07d3aefdd2de6
Ky <ky@noreply.codeberg.org>
2025-05-20 11:08:35 +0200

    Yassified contributions reminder

--- app/[domain]/page.tsx
+++ app/[domain]/page.tsx
@@ -199,13 +199,15 @@ export default async function IndexPage({
             {newHandle ? `"${newHandle}"` : "your new username"}. Finally, tap
             &quot;Verify DNS Record&quot;. Please note that it may take a few minutes for your new handle to be validated.
           </p>
-          <p className="max-w-[700px] text-lg text-muted-foreground sm:text-xl">
-            Registering a domain costs me money. If you would like to contribute, you can{" "}
-            <a href="https://ko-fi.com/kylo" className="underline">
-              help funding this project
-            </a>
-            . ❤️
-          </p>
+          <div className="mt-6 rounded-md bg-muted p-4">
+            <h2 className="max-w-lg font-semibold">
+              Registering a domain costs me money. If you would like to contribute, you can{" "}
+              <a href="https://ko-fi.com/kylo" className="underline">
+                help funding this project
+              </a>
+              . ❤️
+            </h2>
+          </div>
         </Stage>
       </div>
     </main>
